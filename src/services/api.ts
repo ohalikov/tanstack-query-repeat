36e4c717986a4726324478ece119b40a28475825
Олег Халиков <o.halikov@ocas.ai>
2024-03-19 14:33:55 +0500

    fix queries

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,22 +1,21 @@
-import axios from 'axios';
-import { Todo } from '../types/todo';
-import { Project } from '../types/project';
-import { Product } from '../types/product';
+import axios from "axios";
+import { Todo } from "../types/todo";
+import { Project } from "../types/project";
+import { Product } from "../types/product";
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = "http://localhost:8080";
 const axiosInstance = axios.create({ baseURL: BASE_URL });
 
 export const getTodosIds = async () => {
-  return (await axiosInstance.get<Todo[]>('todos')).data.map((todo) => todo.id);
+  return (await axiosInstance.get<Todo[]>("todos")).data.map((todo) => todo.id);
 };
 
 export const getTodo = async (id: number) => {
-  console.log('====> id => ', id);
   return (await axiosInstance.get<Todo>(`todos/${id}`)).data;
 };
 
 export const createTodo = async (data: Todo) => {
-  await axiosInstance.post('todos', data);
+  await axiosInstance.post("todos", data);
 };
 
 export const updateTodo = async (data: Todo) => {
@@ -27,18 +26,19 @@ export const deleteTodo = async (id: number) => {
   await axiosInstance.delete(`todos/${id}`);
 };
 
-export const getProjecs = async (page: number) => {
-    return (await axiosInstance.get<Project[]>(`projects?_page=${page}&_limit=3`)).data
-}
+export const getProjects = async (page = 1) => {
+  return (await axiosInstance.get<Project[]>(`projects?_page=${page}&_limit=3`))
+    .data;
+};
 
-export const getProducts = async ({pageParam}: {pageParam: number}) => {
-    return (
-        await axiosInstance.get<Product[]>(
-            `products?_page${pageParam + 1}&_limit=3`
-        )
-    ).data;
+export const getProducts = async ({ pageParam }: { pageParam: number }) => {
+  return (
+    await axiosInstance.get<Product[]>(
+      `products?_page=${pageParam + 1}&_limit=3`
+    )
+  ).data;
 };
 
 export const getProduct = async (id: number) => {
-    return (await axiosInstance.get<Product>(`products/${id}`)).data;
-  };
+  return (await axiosInstance.get<Product>(`products/${id}`)).data;
+};
--- src/services/queries.ts
+++ src/services/queries.ts
@@ -1,57 +1,66 @@
-import { keepPreviousData, useInfiniteQuery, useQueries, useQuery, useQueryClient } from "@tanstack/react-query"
-import { getProduct, getProducts, getProjecs, getTodo, getTodosIds } from "./api"
-import { Product } from "../types/product"
-
-export function useTodosIds() {
+import {
+    useQuery,
+    useQueries,
+    keepPreviousData,
+    useInfiniteQuery,
+    useQueryClient,
+  } from "@tanstack/react-query";
+  import {
+    getProduct,
+    getProducts,
+    getProjects,
+    getTodo,
+    getTodosIds,
+  } from "./api";
+  import { Product } from "../types/product";
+  
+  export function useTodosIds() {
     return useQuery({
-        queryKey: ['todos'],
-        queryFn: getTodosIds
-    })
-}
-
-// 46:46
-export function useTodos(ids: (number | undefined)[] | undefined) {
-    console.log(ids)
+      queryKey: ["todos"],
+      queryFn: getTodosIds,
+    });
+  }
+  
+  export function useTodos(ids: (number | undefined)[] | undefined) {
     return useQueries({
-        queries: (ids ?? []).map( (id) => {
-            return {
-                queryKey: ['todo', {id}],
-                queryFn: () => getTodo(id!)
-            }
-        })
-    })
-}
-
-export function useProjects(page: number) {
+      queries: (ids ?? []).map((id) => {
+        return {
+          queryKey: ["todo", { id }],
+          queryFn: () => getTodo(id!),
+        };
+      }),
+    });
+  }
+  
+  export function useProjects(page: number) {
     return useQuery({
-        queryKey: ["projects", { page }],
-        queryFn: () => getProjecs(page),
-        placeholderData: keepPreviousData
-    })
-}
-
-export function useProducts() {
+      queryKey: ["projects", { page }],
+      queryFn: () => getProjects(page),
+      placeholderData: keepPreviousData,
+    });
+  }
+  
+  export function useProducts() {
     return useInfiniteQuery({
-        queryKey: ['products'],
-        queryFn: getProducts,
-        initialPageParam: 0,
-        getNextPageParam: (lastPage, _, lastPageParam) => {
-            if (lastPage.length === 0) {
-                return undefined
-            }
-            return lastPageParam + 1
-        },
-        getPreviousPageParam: (_, __, firstPageParam) => {
-            if (firstPageParam <= 1) {
-                return undefined
-            }
-            return firstPageParam - 1
+      queryKey: ["products"],
+      queryFn: getProducts,
+      initialPageParam: 0,
+      getNextPageParam: (lastPage, _, lastPageParam) => {
+        if (lastPage.length === 0) {
+          return undefined;
         }
-
-    })
-}
-
-export function useProduct(id: number | null) {
+        return lastPageParam + 1;
+      },
+      getPreviousPageParam: (_, __, firstPageParam) => {
+        if (firstPageParam <= 1) {
+          return undefined;
+        }
+        return firstPageParam - 1;
+      },
+    });
+  }
+  
+  export function useProduct(id: number | null) {
     const queryClient = useQueryClient();
   
     return useQuery({
